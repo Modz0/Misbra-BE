432e1eb8ed6f15fbb120cc3ebed5c44052bc282f
Modz0 <M7mdj3z@gmail.com>
2025-03-23 16:36:28 +0300

    add project file

---
+++ src/main/java/com/Misbra/MisbraApplication.java
@@ -0,0 +1,13 @@
+package com.Misbra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisbraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisbraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Misbra/MisbraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Misbra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisbraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
